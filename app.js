e8390f663f0fe4e9f366e2e054d713f880330ac3
wlwl0219 <wlwl0219@gmail.com>
2021-03-16 16:44:15 +0900

    Add app.js

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-let test = 'd';
+const canvas = document.querySelector('#jsCanvas');
+const ctx = canvas.getContext('2d');
+const colors = document.querySelectorAll('.jsColor');
+
+canvas.width = 500;
+canvas.height = 500;
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 3;
+
+let painting = false;
+function stopPainting() {
+  painting = false;
+}
+function startPainting() {
+  painting = true;
+}
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+function init() {
+  if (canvas) {
+    canvas.addEventListener('mousemove', onMouseMove);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener('mouseup', stopPainting);
+    canvas.addEventListener('mouseleave', stopPainting);
+  }
+  colors.forEach(color => color.addEventListener('click', handleColorClick));
+}
+init();
